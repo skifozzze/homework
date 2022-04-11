9d42eb51f10c253dbf0c27bdf97aa62964ca70a7
skifozzze <skifozzze@gmail.com>
2022-04-11 14:17:06 +0300

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+// Просимо клієнта підтвердити бронювання готелю
+// і зберігаємо результат роботи confirm у змінну
+const isComing = confirm('Please confirm hotel reservation');
+console.log(isComing);
+
+// Запитуємо назву готеля, в якому хотів би зупинитися клієнт
+// і зберігаємо результат виклику prompt у змінну.
+const hotelName = prompt('Please enter desired hotel name');
+console.log(hotelName);
